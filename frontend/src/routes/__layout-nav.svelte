3c18f428e9ec9975e6dec4dd240f3932f9b31ce7
Victor Buch <47211196+VictorBuch@users.noreply.github.com>
2022-06-29 14:53:12 +0200

    Layout changes

---
+++ frontend/src/routes/__layout-nav.svelte
@@ -0,0 +1,48 @@
+<script>
+	import '../app.css';
+	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import CalendarToday from 'svelte-material-icons/CalendarToday.svelte';
+	import ChartLine from 'svelte-material-icons/ChartLine.svelte';
+	import Noodles from 'svelte-material-icons/Noodles.svelte';
+	import Account from 'svelte-material-icons/Account.svelte';
+</script>
+
+<slot />
+<footer
+	class="bg-accent container fixed bottom-0 flex w-full items-end justify-between rounded-t pt-3 pb-1"
+>
+	<a
+		href="/dashboard"
+		class="fill-accent-content flex flex-col items-center justify-end text-sm font-light"
+	>
+		<CalendarToday size={'23'} color="inherit" />
+		Tracking
+	</a>
+	<a
+		href="/progress"
+		class="fill-accent-content flex flex-col items-center justify-end text-sm font-light"
+	>
+		<ChartLine size={'23'} color="inherit" />
+		Progress
+	</a>
+	<a
+		href="/recipes"
+		class="fill-accent-content flex flex-col items-center justify-end text-sm font-light"
+	>
+		<Noodles size={'23'} color="inherit" />
+		Recipes
+	</a>
+	<a
+		href="/my-account"
+		class="fill-accent-content flex flex-col items-center justify-end text-sm font-light"
+	>
+		<Account size={'23'} color="inherit" />
+		My account
+	</a>
+</footer>
+
+<style>
+	:global(.btn-main) {
+		@apply btn bg-accent btn-block from-accent to-accent-focus text-accent-content shadow-accent/30 mt-8 border-0 bg-gradient-to-r shadow-lg;
+	}
+</style>
---
+++ frontend/src/routes/progress@nav.svelte
@@ -0,0 +1,9 @@
+<script>
+	// your script goes here
+</script>
+
+Stats about your progress will come soon&trade; !
+
+<style>
+	/* your styles go here */
+</style>
