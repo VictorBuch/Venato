c9e7beda89b3d4e6ad01010e49466f39aec47aa9
Victor Buch <47211196+VictorBuch@users.noreply.github.com>
2022-10-03 11:03:26 +0200

    Updated meals type to be accurate to DB meal

--- frontend/src/lib/types/meals.d.ts
+++ frontend/src/lib/types/meals.d.ts
@@ -1,11 +1,17 @@
 export interface Meal {
 	id: number;
 	name: string;
-	description: string;
 	calories: number;
 	carbs?: number;
 	protein?: number;
 	fat?: number;
 	portion: number;
-	portion_unit: string;
+	portion_unit?: string;
+  fat_saturated_g?: number;
+  cholesterol_mg?: number;
+  sodium_mg?: number;
+  potassium_mg?: number;
+  sugar_g?: number;
+  fiber_g?: number;
+  is_quick_tracked: boolean;
 }
